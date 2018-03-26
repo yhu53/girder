9638955b87f911d9be070adcdd4d26384f758de1
Pierre Assemat <pierre.assemat@kitware.com>
2018-03-26 12:43:21 -0400

    Add new endpoint worker status

---
+++ plugins/worker/server/api/worker.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+###############################################################################
+#  Copyright Kitware Inc.
+#
+#  Licensed under the Apache License, Version 2.0 ( the "License" );
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+###############################################################################
+from girder.api import access
+from girder.api.describe import Description, autoDescribeRoute
+from girder.constants import TokenScope
+from girder.api.rest import Resource
+from ..utils import getCeleryApp
+
+
+class Worker(Resource):
+    def __init__(self):
+        super(Worker, self).__init__()
+        self.resourceName = 'worker'
+        self.route('GET', ('status',), self.getWorkerStatus)
+
+    @autoDescribeRoute(
+        Description('Get workers status.')
+        .notes('Note')
+    )
+    @access.user(scope=TokenScope.DATA_READ)
+    def getWorkerStatus(self):
+        app = getCeleryApp()
+        result = {}
+        status = app.control.inspect()
+        result['report'] = status.report()
+        result['stats'] = status.stats()
+        return result
